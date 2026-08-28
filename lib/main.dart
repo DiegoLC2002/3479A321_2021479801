983912e0c86102a4403b4d4ad773b962a80eb81b
DiegoLC2002 <diegolopezcastillo483@gmail.com>
2026-08-28 13:08:15 -0400

    structure_and_assets (1)
    Se descargaron las dependencias del paquete oficial de Flutter llamado Logger para realizar debugs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(printer: PrettyPrinter());
 
 void main() {
+  logger.d('Log message with 2 methods');
   runApp(const MyApp());
 }
 
