1f6dc53cf6542873ec8b7b64a6055f4f4ea1b588
DiegoLC2002 <diegolopezcastillo483@gmail.com>
2026-09-04 12:54:42 -0400

    Creación de Capa de Dominio
    Se creo una capa de dominio llamada foundation.dart que contendra las variables del GameRecord y tanbien se probo a asignar datos y mostrarlas mediante un logger.

--- lib/ui/screens/peg_solitaire_screen.dart
+++ lib/ui/screens/peg_solitaire_screen.dart
@@ -5,9 +5,19 @@ import '../widgets/peg_cell.dart';
 import '../screens/rules_screen.dart';
 import '../screens/about_screen.dart';
 
+import 'package:flutter_laboratorio/models/foundation.dart';
+
 import 'package:logger/logger.dart';
 
 var logger = Logger(printer: PrettyPrinter());
+GameRecord _lastGameRecord = GameRecord(
+  id: '007',
+  date: DateTime(2026, 9, 7, 17, 30),
+  remainingPegs: 5,
+  totalMoves: 18,
+  durationSeconds: 90,
+  isVictory: true,
+);
 
 class PegSolitaireScreen extends StatefulWidget {
   const PegSolitaireScreen({super.key});
@@ -194,6 +204,11 @@ class _PegSolitaireScreenState extends State<PegSolitaireScreen> {
               final CellType cellType = _getCellType(row, col);
 
               final bool isSelected = selectedRow == row && selectedCol == col;
+
+              logger.i(
+                "Último registro de juego:Piezas restantes ${_lastGameRecord.remainingPegs}, "
+                "${_lastGameRecord.durationSeconds} segundos jugados",
+              );
 
               return PegCell(
                 row: row,
